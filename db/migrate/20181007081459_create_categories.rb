41f42e9b5463958a8199ae5d77205afb77bab3c2
141naoto <naot.ishii141@gmail.com>
2018-10-08 16:00:49 +0900

    model3

--- db/migrate/20181007081459_create_categories.rb
+++ db/migrate/20181007081459_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
-      t.string :categoriy_name
+      t.string :category_name
 
       t.timestamps
     end
