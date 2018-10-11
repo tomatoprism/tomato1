0e1f6df9ee23c3c8696ba1873f677ec9d701bd85
Sachiko8 <SachikoSuzuki815@gmail.com>
2018-10-11 07:28:08 +0000

    deviseupdate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_kana, :name, :mail, :phone, :post_code, :address])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:mail])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_kana, :name, :email, :phone, :post_code, :address])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,23 @@
 class Public::UsersController < Public::ApplicationController
 	def show
+		@user = current_user
 
 	end
 
 	def edit
+		@user = User.find(params[:id])
+        if @user != current_user
+            redirect_to user_path
+        end
 
 	end
 
 	def update
-		
+		@user = User.find(params[:id])
+        if @user.update(user_params)
+         redirect_to user_path
+       else
+        redirect_back
+       end
 	end
 end
