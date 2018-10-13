5768e2f4fd489d0b14e8ddd534e28f107982e7ea
山岸宗奨 <yamagisi0714@gmail.com>
2018-10-13 15:32:26 +0900

    Update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,9 +7,12 @@ class Admin::UsersController < Admin::ApplicationController
 		@product_new = Product.new
 	end
 	def create
-		@product_new = Product.save(product_params)
+		@product_new = Product.new(product_params)
+		@product_new.save(product_params)
+		redirect_to admin_user_path(current_admin)
 	end
-		private
+
+	private
 	def product_params
         params.require(:product).permit(:title, :image, :price, :label, :stock, :release, :introduction, :artist_id, :category_id)
    	end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,22 @@
 class CategoriesController < ApplicationController
   def new
+  	@category_new = Category.new
   end
 
   def create
+    @category_new = Category.new(category_params)
+  	@category_new.save(category_params)
+    redirect_to new_category_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:category_name)
   end
 end
