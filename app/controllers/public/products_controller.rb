51afd29b38aea4df03cca911b4f1ea39aa179956
Sachiko8 <SachikoSuzuki815@gmail.com>
2018-10-13 02:48:58 +0000

    update1

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -8,27 +8,7 @@ class Public::ProductsController < Public::ApplicationController
 
 	end
 
-	def edit
-
-	end
-
-	def update
-
-	end
-
-	def destroy
-
-	end
-
-	def new
-
-	end
-
-	def create
-		
-	end
-
 	def product_params
-        params.require(:product).permit(:title, :image_id, :pricd, :label, :stock, :release, :introduction, :artist_id, :category_id)
+        params.require(:product).permit(:title, :image_id, :price, :label, :stock, :release, :introduction, :artist_id, :category_id)
     end
 end
