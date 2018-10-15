461f42e3cafef7145c4cb208b954fa25408f142d
141naoto <naot.ishii141@gmail.com>
2018-10-15 19:38:23 +0900

    product

---
+++ app/controllers/public/tracks_controller.rb
@@ -0,0 +1,18 @@
+class Public::TracksController < ApplicationController
+  def create
+  end
+
+  def update
+  	@track = Track.find(params[:id])
+  	@track.update(track_params)
+  	redirect_to product_path
+  end
+
+  def destroy
+  end
+
+  private
+  def track_params
+    params.require(:track).permit(:music, :list_num, :disc_id)
+  end
+end
