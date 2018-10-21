8b376729038ce05f5f934c52165cb8fb17e9c2b7
山岸宗奨 <yamagisi0714@gmail.com>
2018-10-21 12:57:41 +0900

    update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,11 @@
 class Admin::UsersController < Admin::ApplicationController
+	before_action :correct_user, only:[:index, :show]
+
+	def correct_user
+	    unless admin_signed_in?
+	      redirect_to root_path
+	    end
+  	end
 	def index
 		@users = User.all
 	end
