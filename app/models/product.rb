871a4a0b3abbefd84a43a15102b5c7b243172ff3
山岸宗奨 <yamagisi0714@gmail.com>
2018-10-24 13:14:17 +0900

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -30,6 +30,4 @@ class Product < ApplicationRecord
     validates_presence_of :title
     validates_presence_of :price
     validates_presence_of :image
-    
-
 end
--- db/migrate/20181007081740_create_discs.rb
+++ db/migrate/20181007081740_create_discs.rb
@@ -1,6 +1,7 @@
 class CreateDiscs < ActiveRecord::Migration[5.2]
   def change
     create_table :discs do |t|
+    # create_table :discs, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :product_id
       t.string :disc_name
 
