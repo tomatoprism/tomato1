7e85bfeabb6add76cf21363c1222206c9f37a324
Sachiko8 <SachikoSuzuki815@gmail.com>
2018-10-25 05:18:32 +0000

    update

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -8,7 +8,7 @@ class Public::ProductsController < Public::ApplicationController
   	end
 
 	def index
-		@products = Product.page(params[:page]).order(release: "DESC")
+		@products = Product.page(params[:page]).reverse_order.order(release: "DESC")
 		@categories = Category.all
 		@artists = Artist.all
 	end
